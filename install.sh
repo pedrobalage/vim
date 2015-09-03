b682afe99b5a4a06b1bb47007d7fd396f31148c7
Pedro Balage <pedrobalage@gmail.com>
2015-09-03 14:29:51 -0300

    updating install.sh and bundle

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+# Vim gnome is used to provide +clipboard feature
+sudo apt-get install vim-gnome
+
+# install or update necessary python plugins
 sudo pip install -U jedi flake8 autopep8
-sudo npm -g install jsonlint
+
+# Start and update submodules
 git submodule init 
 git pull --recurse-submodules
 git submodule update --remote --recursive
