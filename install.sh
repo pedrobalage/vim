e683fcf703e5790ef01710e5fac5c3f66567a4f4
Pedro Balage <pedrobalage@gmail.com>
2016-04-22 22:01:22 +0100

    new updates

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 # Vim gnome is used to provide +clipboard feature
 sudo apt-get install vim-gnome
 
-# install or update necessary python plugins
+# install or update necessary python2 plugins (for VIM compiled with +python)
 sudo pip install -U jedi flake8 autopep8
 
-# install or update necessary python3 plugins
+# install or update necessary python3 plugins (for VIM compiled with +python3, like ubuntu 16.04)
 sudo pip3 install -U jedi flake8 autopep8
 
 # Start and update submodules
