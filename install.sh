7491528f3830fea64b48cb687c1d6c131f5cebb7
Pedro Balage <pedro.balage@priberam.pt>
2016-08-24 16:23:48 +0100

    new vimtex module

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 sudo apt-get install vim-gnome
 
 # install or update necessary python2 plugins (for VIM compiled with +python)
-sudo pip install -U jedi flake8 autopep8
+sudo pip install -U jedi flake8 autopep8 git+git://github.com/powerline/powerline
 
 # install or update necessary python3 plugins (for VIM compiled with +python3, like ubuntu 16.04)
-sudo pip3 install -U jedi flake8 autopep8
+sudo pip3 install -U jedi flake8 autopep8 git+git://github.com/powerline/powerline
 
 # Start and update submodules
 git submodule init 
