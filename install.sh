405bd40515b3d63cac35d9ad150915d97a3ecbcc
Pedro Balage <pedrobalage@gmail.com>
2020-07-19 00:06:49 +0100

    update do Ubuntu 20.04

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # install or update necessary python3 plugins (for VIM compiled with +python3, like ubuntu 16.04)
 sudo pip3 install -U jedi flake8 autopep8 git+git://github.com/powerline/powerline
+sudo apt install powerline
 
 # Start and update submodules
 git submodule init 
